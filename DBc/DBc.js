9dab4097b7eb19943fb868858297965a4406cb6a
KBS-james <37357237+KBS-james@users.noreply.github.com>
2020-11-08 22:04:32 +0800

    web display update

---
+++ DBc/DBc.js
@@ -0,0 +1,30 @@
+
+
+var tsHashCountr = []
+for (i=0; i<1000;i++) {tsHashCountr[i]=i}
+console.log(tsHashCountr)
+
+async function intervalFunc() {
+    const options = {
+        method: "POST",
+      };
+    const res = await fetch("/DBCdisplay", options);
+    const disp = await res.json();
+    // console.log(disp);
+    document.getElementById("tick").textContent = disp.cycle;
+    const d = new Date();
+    var str = "" + d;
+    var test = str.substring(4, 24);
+    document.getElementById("time").textContent = test;
+    document.getElementById("count").textContent = disp.cycleHashCount;
+    document.getElementById("hash").textContent = disp.cycleLinkHash
+    for (i = 0; i < 1000; i++) {
+        j = i + 1000;      
+        k = "" + disp.dashboardDisp[i]
+        display = "" + j + ". " + k
+        document.getElementsByTagName("p")[i].textContent = "PS"+display.substring(1) ;
+    }
+        
+
+}
+setInterval(intervalFunc, 1000);
--- index.js
+++ index.js
@@ -1,39 +1,146 @@
 // input hash from PS, upload to DB-t for querry and calculate checksum h256 to RS for sync
 const express = require("express");
 const cors = require('cors');
+var crypto = require("crypto");
 const app = express();
 app.use(cors())
 
 const fetch = require("node-fetch");
-
-app.listen(3200, () => console.log("DB-c-RS1 listening at 127.0.0.1:3200"));
-
+const { stringify } = require("querystring");
+app.use(express.static("DBc"));
 app.use(express.json({ limit: "1mb" }));
-
+DBcID = "RBAS.RS1.DBc0";
+PSID = "RBAS.ps";
 var res1 = {};
-existHash = "ab1234";
+psKeyOriginal = [];
+psKey = [];
+psName = [];
+PsIn = [];
+dashboardDisp = [];
+
+systemTick = 0;
+addHash = 0
+nextChain = "";
+busyFlag = false;
+totalHashToDBcCount = 0;
+dashboardDispHashCount = 0;
+psIn = [];
+psInCount = []
+hashToDBt = [];
+hashToDBtTSname = [];
+for (i = 0; i < 1000; i++) {
+  k = 1000 + i;
+  j = PSID + k;
+  psInCount[i] = 0;
+  psName[i] = j; // j: name to gen initial hash key
+  psKey[i] = crypto.createHash("sha256").update(j).digest("hex");
+  psKeyOriginal[i] = psKey[i]; // initial key
+}
+console.log("default Key table")
+console.log(psName)
+console.log(psKey)
+
+app.listen(3200, () =>
+  console.log("RS1 DB-c0 listening at port 3200" +
+    "\n" + "connect to RS1 localhost:3500" +
+    "\n" + "connect to DB-t at :3400"));
+
+function checkPSname(name, ID) {
+  for (i = 0; i < 1000; i++) {
+    if (name == psName[i]) {
+      if (ID == psKey[i] || ID == psKeyOriginal[i]) {
+        return i;
+      }
+    }
+  }
+  return 1000; // >= 1000
+}
+
+async function timeAdj() {
+  return;
+}
+
 app.post("/hashFile", (req, res) => {
   rxjson = req.body;
-  console.log(rxjson);
-
-  time = Date.now();
-  newHash = rxjson.thash;
-
-  console.log(existHash);
-  console.log(newHash);
-
-  if (newHash === existHash) {
-    res_status = "fail";
-  } else {
-    res_status = "success";
-    existHash = rxjson.thash;
+  // console.log("PS input json");
+  console.log(rxjson.psCycleCount)
+  while (busyFlag == true) {
+    setInterval(timeAdj, 10);
   }
-  console.log(res_status)
-  res.json({
-    status: res_status,
-    hash: existHash,
-    tick: time
-  });
+  psPointer = checkPSname(rxjson.psId, rxjson.chainId);
+  // console.log("tsId =" + rxjson.tsId) //==index of input hash
+  // console.log("tsPointer =" + tsPointer)
+  if (psPointer < 1000) {
+    console.log(psPointer)
+    psIn[psPointer] = rxjson.tsHash;
+    psInCount[psPointer] = rxjson.psCycleCount
+    addHash = addHash + rxjson.psCycleCount //addHash is the object of additional hash/tick
+    j = stringify(rxjson.tsHash)  // next link will be the hash of previous hash object
+    // console.log("addHash = " + addHash)
+    psKey[psPointer] = crypto.createHash("sha256").update(j).digest("hex");
+    result = "submitted";
+  } else {
+    result = "fail: wrong TSname/ID";
+  }
 
+  resJson = {
+    status: result,
+    nextChainId: psKey[psPointer],
+    tick: systemTick,
+  };
+  res.json(resJson);
+  // console.log(resJson)
+}
+);
+
+async function intervalFunc() {
+
+  // console.log("tick = " + systemTick)
+  systemTick++
+  // console.log(addHash)
+  key = "" + systemTick + addHash
+  nextChain = crypto.createHash("sha256").update(key).digest("hex"); // hash to RS
+  dashboardDispHashCount = 0
+  dashboardDisp = [] 
+  console.log(psInCount)    
+  for (i=0; i<1000; i++){
+    dashboardDisp[i] = psInCount[i]
+    // if (dashboardDisp[i] > 1000) {dashboardDisp[i]=dashboardDisp[i]-1000}
+    dashboardDispHashCount = dashboardDispHashCount + dashboardDisp[i]
+  }
+
+  // const options = {
+  //   method: "POST",
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //   },
+  //   body: JSON.stringify(data),
+  // };
+  // res1 = await fetch("http://127.0.0.1:3500/db_c", options) //to RS 
+  // const disp = await res1.json();
+  // console.log(disp)
+  // console.log(disp.status)
+  // result = disp.status
+  // existHash = disp.hash
+}
+
+app.post("/DBCdisplay", async (req, res) => {
+  // console.log("input from PS dashboard")
+  rxjson = req.body;
+  // console.log("PS dashboard req");
+  // console.log(rxjson);
+  while (busyFlag == true) {
+    setInterval(timeAdj, 10); // wait till PS display json is ready
+  }
+  // console.log("PS response to TS");
+  resJson = {
+    cycle: systemTick,
+    cycleHashCount: dashboardDispHashCount,
+    cycleLinkHash: nextChain,
+    dashboardDisp,
+  };
+  res.json(resJson);
+  console.log(resJson);
 });
- 
+
+setInterval(intervalFunc, 1000);
